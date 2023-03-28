fbb2b69c2cc4ac39765ac12f22fbedbb5930bc3c
Sacha <bellier.sacha2@gmail.com>
2023-03-28 02:08:11 +0200

    fix(FMT_CORE) : fix Assert

--- src/ProjectCore/Core/Core.h
+++ src/ProjectCore/Core/Core.h
@@ -3,10 +3,6 @@
 #include <utility>
 #include <cstdint>
 
-#ifdef PROJECTCORE_DEV
-#define PROJECTCORE_DEBUG
-#endif
-
 #ifdef PROJECTCORE_DEBUG
 #define PROJECTCORE_ASSERT_ENABLE
 #define PROJECTCORE_ONLY_ON_DEBUG(x) x
@@ -14,9 +10,13 @@
 #define PROJECTCORE_ONLY_ON_DEBUG(x)
 #endif /* PROJECTCORE_DEBUG */
 
-// TODO : BETTER ASSERT
 #ifdef PROJECTCORE_ASSERT_ENABLE
-	#define PROJECTCORE_ASSERT(x)	if(!(x)) { std::cerr << "ASSERT FAILED! : {}" << #x << std::endl; }
+	#ifdef PROJECTCORE_COMPILER_VS
+		#define PROJECTCORE_ASSERT(x)	if(!(x)) { std::cerr << "ASSERT FAILED! : {}" << #x << std::endl; __debugbreak(); }
+	#else
+		#include <csignal>
+		#define PROJECTCORE_ASSERT(x)	if(!(x)) { std::cerr << "ASSERT FAILED! : {}" << #x << std::endl; std::raise(SIGINT); }
+	#endif
 #else
 	#define PROJECTCORE_ASSERT(x)
 #endif
